b245c63ace4eba503d17477aa1024427faaf60fc
Doneli-web <solofodoneli37@gmail.com>
2026-07-19 23:03:33 +0200

    Sprint-5

--- src/main/java/framework/listener/FrameworkListener.java
+++ src/main/java/framework/listener/FrameworkListener.java
@@ -2,20 +2,23 @@ package framework.listener;
 
 import framework.annotation.Controller;
 import framework.reflection.Utilitaire;
-
 import framework.route.RouteMapping;
 import framework.route.UrlMethod;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @WebListener
 public class FrameworkListener implements ServletContextListener {
+
+    private ApplicationContext springContext;
 
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
@@ -25,8 +28,13 @@ public class FrameworkListener implements ServletContextListener {
         String packageName = context.getInitParameter("packageName");
         String prefixe = context.getInitParameter("prefixe");
         String suffixe = context.getInitParameter("suffixe");
+        String springConfigClass = context.getInitParameter("springConfigClass");
 
         try {
+
+            springContext = new AnnotationConfigApplicationContext(Class.forName(springConfigClass));
+            context.setAttribute("springContext", springContext);
+
             Map<UrlMethod, RouteMapping> urlsMethodes = new HashMap<>();
             Utilitaire.getAllUrlMethode(urlsMethodes, packageName, Controller.class);
             context.setAttribute("urlsMethodes", urlsMethodes);
@@ -40,6 +48,8 @@ public class FrameworkListener implements ServletContextListener {
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-
+        if (springContext != null) {
+            springContext.close();
+        }
     }
 }
--- src/main/java/framework/servlet/FrontControllerServlet.java
+++ src/main/java/framework/servlet/FrontControllerServlet.java
@@ -3,6 +3,7 @@ package framework.servlet;
 import framework.exception.UrlNotFoundException;
 import framework.route.RouteMapping;
 import framework.route.UrlMethod;
+import org.springframework.context.ApplicationContext;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
@@ -12,7 +13,6 @@ import java.io.PrintWriter;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 import framework.reflection.Utilitaire;
 
@@ -23,11 +23,15 @@ public class FrontControllerServlet extends HttpServlet {
     String suffixe;
     private List<String> controllerNames;
     private Map<UrlMethod, RouteMapping> urlsMethodes;
+    private ApplicationContext springContext;
+
     public void init() throws ServletException {
         try {
             this.urlsMethodes = (Map<UrlMethod, RouteMapping>) getServletContext().getAttribute("urlsMethodes");
             this.prefixe = (String) getServletContext().getAttribute("prefixe");
             this.suffixe = (String) getServletContext().getAttribute("suffixe");
+
+            this.springContext = (ApplicationContext) getServletContext().getAttribute("springContext");
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -42,29 +46,27 @@ public class FrontControllerServlet extends HttpServlet {
         String url = request.getRequestURI().substring(contextPath.length());
         UrlMethod urlMethod = new UrlMethod(url, request.getMethod());
 
-        // Résolution AVANT d'ouvrir le writer
         RouteMapping route = Utilitaire.getByUrlAndMethode(urlMethod, urlsMethodes);
         Object controller = route.getClazz().getDeclaredConstructor().newInstance();
-        ModelAndView modelAndView = (ModelAndView) route.getMethod().invoke(controller);
-        for(Map.Entry<String, Object> entry: modelAndView.getAttribute().entrySet()){
-            request.setAttribute(entry.getKey(), entry.getValue());
-        }
-        String view = prefixe + modelAndView.getView() + suffixe;
-        RequestDispatcher requestDispatcher = request.getRequestDispatcher(view);
-        requestDispatcher.forward(request, response);
 
-        try (PrintWriter out = response.getWriter()) {
-            out.println("<h1> Url : " + request.getRequestURI() + "</h1>");
-            out.println("<h3>Méthode associée : <span style='font-weight:normal;'>"
-                    + route.getMethod().getName() + "</span></h3>");
-            out.println("<h4>Methode(Post ou get) : <span style='font-weight:normal;'>"
-                    + urlMethod.getMethode() + "</span></h4>");
-            out.println("<h3>Class associe : <span style='font-weight:normal;'>"
-                    + route.getClazz().getSimpleName() + "</span></h3>");
-            out.println("<h3>Execution de la methode : <span style='font-weight:normal;'>"
-                    + route.getMethod().invoke(route) + "</span></h3>");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        if (springContext != null) {
+            springContext.getAutowireCapableBeanFactory().autowireBean(controller);
+        }
+
+        Object result = route.getMethod().invoke(controller);
+
+        if (result instanceof ModelAndView) {
+            ModelAndView modelAndView = (ModelAndView) result;
+            for (Map.Entry<String, Object> entry : modelAndView.getAttribute().entrySet()) {
+                request.setAttribute(entry.getKey(), entry.getValue());
+            }
+            String view = prefixe + modelAndView.getView() + suffixe;
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher(view);
+            requestDispatcher.forward(request, response);
+        } else {
+            try (PrintWriter out = response.getWriter()) {
+                out.println(String.valueOf(result));
+            }
         }
     }
 
@@ -85,7 +87,7 @@ public class FrontControllerServlet extends HttpServlet {
             processRequest(request, response);
         } catch (UrlNotFoundException | InvocationTargetException | IllegalAccessException | NoSuchMethodException |
                  InstantiationException e) {
-                throw new RuntimeException(e);
+            throw new RuntimeException(e);
         }
     }
 }
